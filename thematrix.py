a403725c0e5fa9641cdabe91dc9fb26e4926ed0a
WebBreacher <WebBreacher@users.noreply.github.com>
2015-10-08 12:17:55 -0400

    Massive changes
    Added food truck and WMATA functions.

--- thematrix.py
+++ thematrix.py
@@ -16,12 +16,15 @@ from datetime import datetime, timedelta
 from pytz import timezone
 import time
 import pytz
+import requests
+import json
 
 ####
 # Variables
 ####
 utc = pytz.utc
 text = ''
+all_food_trucks_combined = set()
 
 ####
 # Methods
@@ -58,7 +61,7 @@ def ImageToMatrixScrollVer(image, direction, speed = 0.05):
             time.sleep(2) # This pauses the display when the content fills the matrix at 0 Y
         else:
             time.sleep(speed) # This controls how fast it scrolls. The longer the delay the slower.
-        matrix.Clear()
+    matrix.Clear()
 
 def CreateImage(text, textwidth = 2200, imgwidth = 2200, textColor = (0, 0, 255)):
     # This creates an image that is then passed to the matrix
@@ -105,6 +108,7 @@ def CreateImage(text, textwidth = 2200, imgwidth = 2200, textColor = (0, 0, 255)
 def TimeZoneText():
     # Create the 'text' string with all the time zones
     global text
+    text = ''
     # Got these from https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
     zones = {'Beijing': 'Asia/Shanghai',
              'Hawaii': 'Pacific/Honolulu',
@@ -120,9 +124,39 @@ def TimeZoneText():
     #text = time.asctime( time.localtime(time.time()) ) # Shows localtime
     for city, zone in sorted(zones.iteritems()):
         zone_time = datetime.now(timezone(zone))
-        text = text + '      ' + city + '->' + zone_time.strftime(fmt)
+        text = text + '      ' + city + ' -> ' + zone_time.strftime(fmt)
     return text
 
+def MetroTrainInfo():
+    requests.packages.urllib3.disable_warnings()
+    headers = { 'api_key': '[APIKEY]'} # Get this from the API area of WMATA's site
+    wmata = requests.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/ N03", headers=headers, verify=False)
+    #print json.dumps(wmata.json(), indent=4)
+    trains = wmata.json()
+    all_trains=""
+    for train in trains['Trains']:
+        if train['Min']=='ARR' or train['Min']=='BRD':
+            state='NOW'
+        else:
+            state='%s mins' % train['Min']
+        if train['DestinationName']=='No Passenger' or train['Car']=='-':
+        continue
+        all_trains = all_trains+'%s going to %s --- ' % (state, train['DestinationName'])
+    all_trains_lst=[]
+    all_trains_lst=all_trains.strip(" --- "),str(len(all_trains))
+    return all_trains_lst
+
+def FoodTrucks():
+    # May have to submit a different User-Agent or the RANDOM in the variable
+    food_trucks = requests.get('http://foodtruckfiesta.com/apps/map_json.php')
+    all_food_trucks = food_trucks.json()
+    for truck in all_food_trucks['markers']:
+        if str(truck['coord_lat'])[:5] == '38.92':
+            all_food_trucks_combined.add(truck['print_name'])
+    food_trucks_out =  ', '.join(sorted(all_food_trucks_combined))
+    return food_trucks_out
+
+            
 ####
 # Create
 ####
@@ -132,15 +166,25 @@ matrix = Adafruit_RGBmatrix(32, 4)
 # Call the Function(s) to create content and write this to the Matrix
 ####
 loop = 0
-while loop == 0:
-    #MatrixFill(0x00AA00, 1)
-    image = CreateImage('-> Welcome <-', 124, 124, (180, 180, 180))
-    ImageToMatrixScrollVer(image, 'down', 0.02)
-    image = CreateImage('Welcome to the this program!', 400, 400, (0, 0, 255))
-    ImageToMatrixScrollHor(image, 'r2l', 0.02)
+while loop==0: 
+    #MatrixFill(0x00AA00, 1) #Fill the matrix with a single color
+    #image = CreateImage('-> TACREP <-', 124, 124, (180, 0, 0))
+    #ImageToMatrixScrollVer(image, 'down', 0.02)
+    image = CreateImage('Welcome to the TACREP CSOC.', 400, 400, (0, 0, 255)) #Blue
+    ImageToMatrixScrollHor(image, 'r2l', 0.01)
 
+    image = CreateImage('Food Trucks', 124, 124, (180, 180, 180))
+    ImageToMatrixScrollVer(image, 'up', 0.02)
+    image = CreateImage(FoodTrucks(), len(FoodTrucks())*7, len(FoodTrucks())*7, (180, 0, 0)) #Red
+    ImageToMatrixScrollHor(image, 'r2l', 0.01)
+    
+    image = CreateImage('Greensboro Metro', 124, 124, (180, 180, 180))
+    ImageToMatrixScrollVer(image, 'up', 0.02)
+    metro = MetroTrainInfo()
+    image = CreateImage(metro[0], int(metro[1])*9, int(metro[1])*9, (0, 180, 0)) # Green
+    ImageToMatrixScrollHor(image, 'r2l', 0.01)
+    
     image = CreateImage('World Time', 124, 124, (180, 180, 180))
     ImageToMatrixScrollVer(image, 'up', 0.02)
-    image = CreateImage(TimeZoneText(), 2300, 2300, (180, 0, 180))
-    ImageToMatrixScrollHor(image, 'r2l', 0.02)
-    #loop += 1
+    image = CreateImage(TimeZoneText(), 2300, 2300, (180, 0, 180)) #Purple
+    ImageToMatrixScrollHor(image, 'r2l', 0.01)
